28eec99f9564c733451d043eead2e04a80e20072
xiaoxin <3115242940@qq.com>
2026-04-12 13:53:47 +0800

    change database config

--- MyWebsiteBackend/settings.py
+++ MyWebsiteBackend/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.mysql'),
         'NAME': os.environ.get('DB_NAME', 'my_website'),
-        'USER': os.environ.get('DB_USER', 'user'),
+        'USER': os.environ.get('DB_USER', 'root'),
         'PASSWORD': os.environ.get('DB_PASSWORD', '123456'),
         'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DB_PORT', '3306'),
